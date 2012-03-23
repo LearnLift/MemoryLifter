b171c3253d5f0524b5535d89d017c2161b2a2a51
CFI_cp <SND\CFI_cp>
2012-03-23 14:19:54 +0000

    version bump: 2.5

--- Development/Current/MLifter.CardCollector/Properties/AssemblyInfo.cs
+++ Development/Current/MLifter.CardCollector/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.1.0")]
-[assembly: AssemblyFileVersion("2.4.1.0")]
-[assembly: AssemblyInformationalVersion("2.4.1.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
